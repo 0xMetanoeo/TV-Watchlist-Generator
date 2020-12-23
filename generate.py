3a930776f6a448f6fc5445c13c8956762668c954
MetaNoeo <metanoeo2048@protonmail.com>
2020-12-23 00:04:24 -0600

    daily generation

--- generate.py
+++ generate.py
@@ -2,6 +2,8 @@ import os
 import sys
 import ccxt
 from pathlib import Path
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 
 # Example
 # BINANCE:BTCUSDTPERP
@@ -11,18 +13,20 @@ exchange_types = ['spot','future']
 
 def write_watchlist(exchange, ex_type):
     print("fetching for ", exchange)
-    tickers = get_active_tickers(exchange, ex_type)
+    tickers = get_active_tickers(exchange, ex_type)    
     if tickers:
         file_name = f"{exchange}_{ex_type}.txt"
         out_file_with_ex = Path(output_path,'exchange_embedded_symbols', file_name)
         out_file_without_ex = Path(output_path, 'generic_symbols', file_name)
         with open(out_file_with_ex, 'w') as f:
             for symbol in tickers:
-                f.write(f"{exchange.upper()}:{symbol}\n")
+                if (exchange == 'binance') and (ex_type == 'future'):
+                        f.write(f"{exchange.upper()}:{symbol}PERP\n")
+                else:
+                    f.write(f"{exchange.upper()}:{symbol}\n")
         with open(out_file_without_ex, 'w') as f:
             for symbol in tickers:
                 f.write(f"{symbol}\n")
-        
     else:
         print(f"Sorry, couldn't connect to: {exchange}_{ex_type}")
 
